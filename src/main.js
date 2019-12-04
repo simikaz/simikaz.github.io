7944e8c640f27087ec73cbd1140a08be68b5f01c
清水 一憲 <shimizu@kazunori-353.local>
2019-12-05 02:48:32 +0900

    hoge

--- src/main.js
+++ src/main.js
@@ -11,3 +11,23 @@ new Vue({
             .then(response => (this.info = response.data))
     }
 })
+
+new Vue({
+    el: '#app_time',
+    data() {
+        //       now: "00:00:00"
+    },
+    computed: {
+        time: function () {
+            var date = new Date();
+            var year = date.getFullYear();
+            var month = ("0" + (date.getMonth() + 1)).slice(-2);
+            var day = ("0" + date.getDate()).slice(-2);
+            var hour = ("0" + date.getHours()).slice(-2);
+            var minute = ("0" + date.getMinutes()).slice(-2);
+            var second = ("0" + date.getSeconds()).slice(-2);
+            return this.time = hour + ":" + minute + ":" + second
+            //return this.time = year + "/" + month + "/" + day
+        }
+    }
+})

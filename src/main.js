6b0264f4864d5e883feb0df75ee51e1336451704
清水 一憲 <shimizu@kazunori-353.local>
2019-12-05 02:52:57 +0900

    shimizu

--- src/main.js
+++ src/main.js
@@ -26,7 +26,7 @@ new Vue({
             var hour = ("0" + date.getHours()).slice(-2);
             var minute = ("0" + date.getMinutes()).slice(-2);
             var second = ("0" + date.getSeconds()).slice(-2);
-            return this.time = hour + ":" + minute + ":" + second
+            return this.time = hour //+ ":" + minute + ":" + second
             //return this.time = year + "/" + month + "/" + day
         }
     }
